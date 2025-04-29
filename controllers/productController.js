0d6b3d2efb5c4eb9b4720bb3f86c4097a9681c78
Mahera <maherakeshan90@gmail.com>
2025-04-29 20:24:34 +0530

    Get product By ID 🪪

--- controllers/productController.js
+++ controllers/productController.js
@@ -103,4 +103,36 @@ export async function updateProduct(req, res){
 			}
 	}
 
+	//get product by id
+export async function getProductById(req, res){
+	const productId = req.params.productId
 
+	try{
+		const product = await Product.findOne({productId: productId})
+		if(product == null){
+			res.status(404).json({
+				message: "Product not found"
+			})
+			return
+		}
+		if(product.isAvailable){
+			
+			res.json(product)
+		}else{
+			if(!isAdmin(req)){
+				res.status(404).json({
+					message: "Product not found"
+				})
+				return
+			}else{
+				res.json(product)
+			}	
+			
+			}
+	}catch(err){
+				res.status(500).json({
+					message: "Failed to get product",
+					error: err
+				})
+			}
+}
